9a2980b149ca71783cc3b7290f08bf1c2ce213a3
Eva <avociafuture@gmail.com>
2025-12-04 13:58:16 +0300

    categories added

--- config/webpack.pages.js
+++ config/webpack.pages.js
@@ -17,6 +17,12 @@ const htmlPages = [
   createPage('./src/pages/dictionary/dictionary.html', './pages/dictionary/dictionary.html', ['index']),
   createPage('./src/pages/errors/404.html', './pages/errors/404.html', ['index']),
   createPage('./src/pages/articles/finance/onlinepayments.html', './pages/articles/finance/onlinepayments.html', ['index']),
+  createPage('./src/pages/articles/lifestyle/lifestyle.html', './pages/articles/lifestyle/lifestyle.html', ['index']),
+  createPage('./src/pages/articles/home/home.html', './pages/articles/home/home.html', ['index']),
+  createPage('./src/pages/articles/health/health.html', './pages/articles/health/health.html', ['index']),
+  createPage('./src/pages/articles/finance/finance.html', './pages/articles/finance/finance.html', ['index']),
+  createPage('./src/pages/articles/documents/documents.html', './pages/articles/documents/documents.html', ['index']),
+  createPage('./src/pages/articles/career/career.html', './pages/articles/career/career.html', ['index']),
   createPage('./src/pages/styleguide.html', './pages/styleguide.html', ['styleguide'])
 ];
 
