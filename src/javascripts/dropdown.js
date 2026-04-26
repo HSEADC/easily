32bd10b3d9095c77f4ac1be77feda81ab6856bcc
Eva <avociafuture@gmail.com>
2026-04-26 21:02:20 +0300

    added menu  icon rotation animation

--- src/javascripts/dropdown.js
+++ src/javascripts/dropdown.js
@@ -2,11 +2,15 @@
 
 const menuTrigger = document.querySelectorAll('.A_menu_button');
 const dropDown = document.querySelectorAll('.M_nav_items');
+const icon = document.querySelectorAll('.dropdown_icon');
 
 menuTrigger.forEach(btn => {
   btn.addEventListener('click', () => {
     dropDown.forEach(item => {
       item.classList.toggle('active');
     });
+    icon.forEach(i => {
+      i.classList.toggle('active');
+    })
   });
 });
