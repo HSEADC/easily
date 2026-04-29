47a687d36775717d834db08af0d9ec4f34fb2997
Eva <avociafuture@gmail.com>
2026-04-30 02:44:16 +0300

    dictionary search function, rearranged html file a bit, some new shitty styles

--- src/pages/dictionary/dictionary.js
+++ src/pages/dictionary/dictionary.js
@@ -12,3 +12,62 @@ cards.forEach(card => {
     card.classList.toggle('flipped');
   })
 });
+
+// glossary search
+
+const terms = [
+  {
+    title: 'ОМС',
+    id: 'oms'
+  },
+  {
+    title: 'Выгорание',
+    id: 'burnout'
+  },
+  {
+    title: 'СНИЛС',
+    id: 'snils'
+  },
+  {
+    title: 'Залог',
+    id: 'zalog'
+  },
+  {
+    title: 'МРОТ',
+    id: 'mrot'
+  },
+  {
+    title: 'Бюджет',
+    id: 'budget'
+  },
+];
+
+search();
+
+function search() {
+  const input = document.querySelector('.glossary_search');
+
+  input.addEventListener('input', () => {
+    handleSearchInput(terms, input);
+  });
+}
+
+function handleSearchInput (terms, input) {
+  const value = input.value.toLowerCase();
+  const glossaryCards = document.querySelector('.C_glossary_list');
+  const results = terms.filter(term => term.title.toLowerCase().includes(value));
+
+  if (results > 0) {
+    glossaryCards.children.style.display = 'none';
+  }
+  if (value.length >= 2) {
+    renderSearchResults(results, value);
+  }
+}
+
+function renderSearchResults(results, value) {
+  results.forEach(res => {
+    const card = document.getElementById(`${res.id}`);
+    card.style.display = 'flex';
+  });
+}
