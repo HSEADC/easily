35242b26f0ec382b96aba6ad1b215f545c8292ca
Eva <avociafuture@gmail.com>
2026-06-06 23:31:18 +0300

    created header element

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -86,6 +86,12 @@ module.exports = {
       priority: 'replace'
     }),
     new HtmlWebpackPartialsPlugin({
+      path: path.join(__dirname, '../src/partials/header.html'),
+      location: 'headerPartial',
+      template_filename: '*',
+      priority: 'replace'
+    }),
+    new HtmlWebpackPartialsPlugin({
       path: path.join(__dirname, '../src/partials/analitics.html'),
       location: 'analitics',
       template_filename: '*',
