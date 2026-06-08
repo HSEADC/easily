e0cb4f1545cbb5be0d27e98e44a2f527e8ef7800
Eva <avociafuture@gmail.com>
2026-06-08 22:47:41 +0300

    created links data and changed classnames

---
+++ src/javascripts/menubarData.js
@@ -0,0 +1,17 @@
+const homeURL = 'http://localhost:8080/pages';
+// const homeURL = 'https://easily.adc.ac/pages';
+
+const menu = [
+  {
+    text: 'статьи',
+    url: 'articles/articles.html'
+  },
+  {
+    text: 'гайды',
+    url: 'guides/guides.html'
+  },
+  {
+    text: 'словарь',
+    url: 'dictionary/dictionary.html'
+  },
+];
